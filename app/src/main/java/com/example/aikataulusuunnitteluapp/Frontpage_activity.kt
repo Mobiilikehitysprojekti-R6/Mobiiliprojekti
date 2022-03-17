05fd80a1f78990aadaa268fdcf91b4cd689731d4
t9musa <t9musa00@students.oamk.fi>
2022-03-17 14:09:46 +0200

    muutama melko tyhja aktiviteetti lisatty

---
+++ app/src/main/java/com/example/aikataulusuunnitteluapp/Frontpage_activity.kt
@@ -0,0 +1,38 @@
+package com.example.aikataulusuunnitteluapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Frontpage_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frontpage)
+
+        var btn_showCalendar = findViewById<Button>(R.id.btn_toCalendar)
+
+        //actionbar
+        val actionbar = supportActionBar
+        //set actionbar title
+        actionbar!!.title = "Frontpage"
+        //set back button
+        actionbar.setDisplayHomeAsUpEnabled(true)
+        actionbar.setDisplayHomeAsUpEnabled(true)
+
+
+        btn_showCalendar.setOnClickListener {
+            startActivity(Intent(this@Frontpage_activity, Calendar_activity::class.java))
+        }
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+
+
+}
--- app/src/main/java/com/example/aikataulusuunnitteluapp/MainActivity.kt
+++ app/src/main/java/com/example/aikataulusuunnitteluapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.aikataulusuunnitteluapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btn_login = findViewById<Button>(R.id.btn_login)
+
+        btn_login.setOnClickListener{
+            startActivity(Intent(this@MainActivity, Frontpage_activity::class.java))
+        }
+
+
+
     }
 }
