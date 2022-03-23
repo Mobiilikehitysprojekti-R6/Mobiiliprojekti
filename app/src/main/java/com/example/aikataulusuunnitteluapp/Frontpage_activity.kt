7f6b4aa43efd090368e0140482d4ff1a506110bc
t9musa <t9musa00@students.oamk.fi>
2022-03-23 11:51:08 +0200

    lisatty tasatunnit+muuttuva timestamp

--- app/src/main/java/com/example/aikataulusuunnitteluapp/Frontpage_activity.kt
+++ app/src/main/java/com/example/aikataulusuunnitteluapp/Frontpage_activity.kt
@@ -45,7 +45,6 @@ class Frontpage_activity : AppCompatActivity(), PopupMenu.OnMenuItemClickListene
         // Use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         recyclerView.setHasFixedSize(true)
-
         initWidgets()
         selectedDate = LocalDate.now()
         setDates()
